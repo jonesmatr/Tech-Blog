60f0b0a8a6ee3a77450460ca12db53663e4a7e6a
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-22 18:19:15 -0400

    Linked css to handlebars and started adding some event listeners to the public folder.

---
+++ public/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here. For example:
+    console.log("Client-side script loaded!");
+
+    // Add event listeners, manipulate the DOM, etc.
+});
