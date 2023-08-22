12e21e2b1c4e994cf51b11caa3b5ea7a70b80710
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-22 13:16:47 -0400

    Set up the postRoutes. I will build this out later.

---
+++ controllers/postRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { Post, Comment } = require('../models');
+
+router.post('/create', async (req, res) => {
+    // Logic for creating a new post
+    // ...
+});
+
+router.put('/edit/:id', async (req, res) => {
+    // Logic for editing a post with a specific ID
+    // ...
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    // Logic for deleting a post with a specific ID
+    // ...
+});
+
+router.post('/:id/comment', async (req, res) => {
+    // Logic for adding a comment to a specific post
+    // ...
+});
+
+module.exports = router;
