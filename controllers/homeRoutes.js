abc0aa6541b2b15edfca73f2a15e42a07ca0d8ee
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-22 13:15:22 -0400

    Set the homeRoutes.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { Post } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.render('home', { posts: postData });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const post = await Post.findByPk(req.params.id);
+        if (post) {
+            res.render('post', { post });
+        } else {
+            res.status(404).send('Post not found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
