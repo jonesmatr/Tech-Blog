dcaa477b4d316350bec487c6c1f6fbc7699e1c75
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-22 13:07:42 -0400

    Updated the .gitignore and built out the server,js file.

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers'); // Assuming your routes are in a "controllers" folder
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware to parse JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static directory
+app.use(express.static('public'));
+
+// Handlebars setup
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Session setup
+const sess = {
+  secret: 'supersecret',  // Normally, this should be a complex string or pulled from .env
+  cookie: { maxAge: 7200000 },  // 2 hours
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
+// Activate routes
+app.use(routes);
+
+// Connect to the database and then start the Express server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
