17c1cda5686caa056e4f6c7c1a9e70da504ba378
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-24 12:47:50 -0400

    Fixed issue with being able to see dashboard after logging out.

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const logoutRoutes = require('./logoutRoutes');
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 
 // Route for rendering the dashboard
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
   // Handle the request and render a view or return data
   res.render('dashboard');
+} catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 // Route for handling logout
