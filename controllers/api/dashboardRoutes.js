f76afb6ed4237f533df8678de1bac643e5527fe4
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-24 10:35:30 -0400

    Fixed the logout button.

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -16,5 +16,4 @@ router.post('/logout', (req, res) => {
     }
 });
 
-
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
         req.session.user_id = user.id;
         req.session.logged_in = true;
 
-        res.json({ user: user, message: 'You are now logged in!' });
+        res.redirect('/dashboard');
     } catch (err) {
         res.status(500).json(err);
     }
