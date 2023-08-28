b232032aebd4f129af6ab16f1ecb152ec3d5ab76
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-28 13:53:28 -0400

    Changed path.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -82,7 +82,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post('/api/users/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
 
