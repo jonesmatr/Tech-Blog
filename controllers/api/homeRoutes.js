b29544803ca882cae1f3bcbd24a7ccf73d8715c1
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-06 07:13:49 -0400

    Changed login.js. Removed duplicate signup form.

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -64,6 +64,7 @@ router.post('/signup', async (req, res) => {
 
         res.redirect('/login'); // Redirect to the login page
     } catch (error) {
+        console.error(error);
         res.status(500).json({ message: 'An error occurred during sign-up.' });
     }
 });
