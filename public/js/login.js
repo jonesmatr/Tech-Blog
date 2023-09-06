4b1d8d7fcd591aa06f291495d402bc0a62917e90
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-06 06:59:44 -0400

    Changed login.js. Removed duplicate signup form.

--- public/js/login.js
+++ public/js/login.js
@@ -50,26 +50,26 @@ document.addEventListener('DOMContentLoaded', () => {
   signupForm.addEventListener('submit', signupFormHandler);
 });
 
-const loginFormHandler = async (event) => {
-  event.preventDefault();
+// const loginFormHandler = async (event) => {
+//   event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+//     const username = document.querySelector('#username-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+//     if (username && password) {
+//         const response = await fetch('/api/users/login', {
+//             method: 'POST',
+//             body: JSON.stringify({ username, password }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
-};
+//         if (response.ok) {
+//             document.location.replace('/dashboard');
+//         } else {
+//             alert(response.statusText);
+//         }
+//     }
+// };
 
 // const signupFormHandler = async (event) => {
 //   event.preventDefault();
