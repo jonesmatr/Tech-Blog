392e931cc7f6cc730cd29b777e5baf3f55273663
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-06 10:29:46 -0400

    Changed login.js. Removed duplicate signup form.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const { Post, User, Comment } = require('../../models');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./api/userRoutes');
 const postRoutes = require('./api/postRoutes');
-const homeRoutes = require('./api/homeRoutes');
+const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./api/dashboardRoutes');
 
 router.use('/api/users', userRoutes);
