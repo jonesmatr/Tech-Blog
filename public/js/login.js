422a0dd38fb5e2e69235a03f78dbbafac462b2f8
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-06 16:47:19 -0400

    Working on signup form

--- public/js/login.js
+++ public/js/login.js
@@ -78,7 +78,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && password) {
-      const response = await fetch('/api/users/signup', {
+      const response = await fetch('/signup', {
           method: 'POST',
           body: JSON.stringify({ username, password }),
           headers: { 'Content-Type': 'application/json' },
