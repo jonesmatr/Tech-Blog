2355a2ab9017f9adf540c57564a350ad237225a8
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-06 10:44:46 -0400

    Changed login.js. Removed duplicate signup form.

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,23 @@
-const router = require('express').Router();
-const userRoutes = require('./api/userRoutes');
-const postRoutes = require('./api/postRoutes');
-const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./api/dashboardRoutes');
+// const router = require('express').Router();
+// const userRoutes = require('./api/userRoutes');
+// const postRoutes = require('./api/postRoutes');
+// const homeRoutes = require('./homeRoutes');
+// const dashboardRoutes = require('./api/dashboardRoutes');
 
-router.use('/api/users', userRoutes);
-router.use('/api/posts', postRoutes);
-router.use('/api/dashboard', dashboardRoutes);
-router.use('/', homeRoutes); 
+// router.use('/api/users', userRoutes);
+// router.use('/api/posts', postRoutes);
+// router.use('/api/dashboard', dashboardRoutes);
+// router.use('/', homeRoutes); 
+
+// module.exports = router;
+
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@
       const password = document.querySelector('#password-login').value.trim();
 
       if (username && password) {
-          const response = await fetch('/login', {
+          const response = await fetch('/api/users/login', {
               method: 'POST',
               body: JSON.stringify({ username, password }),
               headers: { 'Content-Type': 'application/json' },
@@ -27,7 +27,7 @@
       const password = document.querySelector('#password-signup').value.trim();
 
       if (username && password) {
-          const response = await fetch('/signup', {
+          const response = await fetch('/api/users', {
               method: 'POST',
               body: JSON.stringify({ username, password }),
               headers: { 'Content-Type': 'application/json' },
