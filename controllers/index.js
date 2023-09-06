01fa7600dd79dbfebfbacdd6c54a48d5eb36648b
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-06 10:54:28 -0400

    Changed login.js. Removed duplicate signup form.

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,15 @@
-// const router = require('express').Router();
-// const userRoutes = require('./api/userRoutes');
-// const postRoutes = require('./api/postRoutes');
-// const homeRoutes = require('./homeRoutes');
-// const dashboardRoutes = require('./api/dashboardRoutes');
-
-// router.use('/api/users', userRoutes);
-// router.use('/api/posts', postRoutes);
-// router.use('/api/dashboard', dashboardRoutes);
-// router.use('/', homeRoutes); 
-
-// module.exports = router;
-
-
 const router = require('express').Router();
-
-const apiRoutes = require('./api');
+const userRoutes = require('./api/userRoutes');
+const postRoutes = require('./api/postRoutes');
 const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
+const dashboardRoutes = require('./api/dashboardRoutes');
 
-router.use('/', homeRoutes);
+router.use('/api/users', userRoutes);
+router.use('/api/posts', postRoutes);
+router.use('/api/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes); 
 
 module.exports = router;
+
